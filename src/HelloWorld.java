9c370e6ecdf98a169da9fcbb461683b4ebb230bd
suleymanDagtekin <simpleveryeasy@gmail.com>
2025-04-14 22:25:48 +0300

    work done after years

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,5 +8,10 @@ public class HelloWorld {
 
     // changes to third commit (4th commit)
 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Bye Bye World");
+    }
+
 
 }
